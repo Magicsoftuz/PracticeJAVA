c0c7f51b2572495f0cfccabbec969c44d482c4a5
MagicSoft <magicsoft.uz@gmail.com>
2022-07-30 21:24:49 +0500

    Expressions

---
+++ expressions/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args){
+        // expression = operands & operators
+        // operands = values, variables, numbers, quantity
+        // operators = +, - , /, *, %
+
+        int friends = 10;
+        friends = friends +10;
+        System.out.println("Add: "+ friends);
+
+        friends = friends - 5;
+        System.out.println("Minus: "+friends);
+
+        friends = friends *2;
+        System.out.println("Multiple: "+friends);
+
+        friends = friends /3;
+        System.out.println("Division: "+ friends);
+
+        friends = friends%4;
+        System.out.println("Remandir: "+ friends);
+
+    }
+}
