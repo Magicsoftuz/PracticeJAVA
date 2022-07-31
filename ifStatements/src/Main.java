02e4cb8ffbef8a16fddb6b7a17817c0b3fdb5e4e
MagicSoft <magicsoft.uz@gmail.com>
2022-07-31 09:44:27 +0500

    Finish switch Statements

--- ifStatements/src/Main.java
+++ ifStatements/src/Main.java
@@ -1,19 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-       int age = 40;
 
-       if (age>75){
-           System.out.println("You are Boomer!");
-       }
-       else if (age>40){
-           System.out.println("You are pensioner");
-       }
-       else if (age>18){
-           System.out.println("You are adult");
-       }
+        String day = "Friday";
+        switch (day){
+            case "Monday":
+                System.out.println("Today is Monday!");
+                break;
+            case "Tuesday":
+                System.out.println("Today is Tuesday!");
+                break;
+            case "Wenesday":
+                System.out.println("Today is Wenesday!");
+                break;
+            case "Thursday":
+                System.out.println("Today is Thursday!");
+                break;
+            case "Friday":
+                System.out.println("Today is Friday!");
+                break;
+            case "Sutarday":
+                System.out.println("Today is Sutarday!");
+                break;
+            case "Sunday":
+                System.out.println("Today is Sunday!");
+                break;
+            default:
+                System.out.println("It is not week day");
+        }
 
-       else {
-           System.out.println("You are a baby");
-       }
     }
 }
