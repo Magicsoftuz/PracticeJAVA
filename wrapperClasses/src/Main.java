af4ab26ad1753bad1485163049535b80bc2ab80f
MagicSoft <magicsoft.uz@gmail.com>
2022-07-31 11:56:25 +0500

    Finished Wrapper classes

---
+++ wrapperClasses/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /* Wrapper classes =  1) Provides a way to use primitive data types as reference data types
+                              2) Reference data types contain useful methods
+                              3) Can be used with collections (example: ArrayList)
+
+          Primitive     Wrapper
+          ---------     ----------
+          1) boolean    1)Boolean
+          2) char       2)Character
+          3) int        3)Integer
+          4) double     4)Double
+          5) ....       5) ....
+         */
+
+        Boolean a = true;
+//        Character b = "A";
+        Integer c= 100;
+        Double d = 3.14;
+        String e = "Umid";
+
+
+        System.out.println(a+" "+ c+" "+ d+" "+ e);
+    }
+}
