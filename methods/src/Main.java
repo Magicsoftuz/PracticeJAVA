ba5d7bf869f1a74752663e5a534ba7f0108af6ba
MagicSoft <magicsoft.uz@gmail.com>
2022-08-01 09:03:26 +0500

    Finished methods

---
+++ methods/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+//        hello("Umid", 20);
+        int x = 4;
+        int y=5;
+        System.out.println(add(x,y));
+    }
+//    static void hello(String name, int age){
+//        System.out.println("Hello "+name);
+//        System.out.println("you are "+age);
+//    }
+    static  int add(int x, int y){
+        return x+y;
+    }
+
+}
