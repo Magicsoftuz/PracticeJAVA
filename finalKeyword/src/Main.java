96dbbbc47ed828fa173d5b8b49163a6f200d151b
MagicSoft <magicsoft.uz@gmail.com>
2022-08-01 09:52:08 +0500

    Finished final keyword

---
+++ finalKeyword/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        final double PI = 3.14159; // keyinchalik PI variable ni uzgartirib bulmaydi
+       // PI = 22; // Mumkin emas
+        System.out.println(PI);
+    }
+}
