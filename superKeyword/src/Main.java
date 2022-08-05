7d81d45f60a6941d581fbad7041633ef3fce8cfb
MagicSoft <magicsoft.uz@gmail.com>
2022-08-05 10:22:02 +0500

    Finished super keyword

---
+++ superKeyword/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Hero hero = new Hero("Umid", 20, "Super Mind");
+
+//        System.out.println(hero.name);
+//        System.out.println(hero.age);
+//        System.out.println(hero.power);
+
+        System.out.println(hero.toString());
+    }
+}
